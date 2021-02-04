9b430add474af71199e4ca35b9cb5af8a4c6bc52
freethinkingzen <johnlewis208503@gmail.com>
2021-02-04 11:25:33 -0800

    aded command line functionality

--- MonteHall.py
+++ MonteHall.py
@@ -1,13 +1,53 @@
+#!/usr/bin/python
 
 # This program test the results of the Monte Hall Problem
 # since the results are counterintuitive to most people
 
+import sys
+import argparse
 import random
 
 
-ROUNDS = 1000
+ROUNDS = 1000 # Default number of rounds
+
+helpMessage = '''
+** HELP MENU **
+===============================================================================
+
+    usage: MonteHall.py [-h, --help] [ROUNDS]
+
+    optional arguments:
+        -h, --help      display this help message and exit
+        [ROUNDS]        Number of times to repeat the Monte Hall Problem
+                        Must be integer value betwwen 1 and 10,000
+
+================================================================================
+    '''
+
+
+def parseArgs():
+    args = len(sys.argv)
+    if args == 2:
+        arg1 = sys.argv[1]
+        if(arg1 == "--help" or arg1 == "-h"):
+            print(helpMessage)
+            exit()
+        elif(arg1.isdigit() and int(arg1) in range(1,10001)):
+            return int(arg1)
+        else:
+            print("\nError: invalid argument at position 1, '", arg1, "'. See usage below:")
+            print(helpMessage)
+            exit()
+    if args > 2:
+        print("\nError: invalid number of arguments, '", len(argv), "'. See usage below:")
+        print(helpMessage)
+        exit()
+
+    return ROUNDS
+
 
 if __name__ =="__main__":
+    ROUNDS = parseArgs()
 
     keep_door = 0       # Successful rounds when keeping door choice
     switch_door = 0     # Successful rounds when switching door choice
